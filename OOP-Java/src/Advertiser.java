2ba7147b7871aeeab5c4262659fa2542fb6f232e
Masihbr <masihbr@gmail.com>
2021-01-07 18:31:39 +0330

    fixed help method

--- OOP-Java/src/Advertiser.java
+++ OOP-Java/src/Advertiser.java
@@ -22,12 +22,12 @@ public class Advertiser extends BaseAdvertising {
     }
 
     public static String help() {
-        return "Advertiser{" +
-                +'\t' + "String name: contains name of the Advertiser" + '\n' +
-                +'\t' + "int id: a unique number to identify Advertiser" + '\n' +
-                +'\t' + "int clicks: contains number of clicks that Advertiser has received" + '\n' +
-                +'\t' + "int views: contains number of clicks that Advertiser has received" + '\n' +
-                '}';
+        return "Advertiser{" + '\n'
+                + "     " + "String name: contains name of the Advertiser" + '\n'
+                + "     " + "int id: a unique number to identify Advertiser" + '\n'
+                + "     " + "int clicks: contains number of clicks that Advertiser has received" + '\n'
+                + "     " + "int views: contains number of clicks that Advertiser has received" + '\n'
+                + '}';
     }
 
     /*  alternative help (if the fields are gonna change over time)
