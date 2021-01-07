76eef2a91e3fb31157c0d98c2b5bf1d9547461f6
Masihbr <masihbr@gmail.com>
2021-01-07 16:50:23 +0330

    Advertiser initial setup

--- OOP-Java/src/Advertiser.java
+++ OOP-Java/src/Advertiser.java
@@ -1,2 +1,46 @@
+import java.lang.reflect.Field;
+
 public class Advertiser {
+    private static int totalClicks;
+    private int id;
+    private String name;
+    private int clicks;
+    private int views;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getClicks() {
+        return clicks;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void incClicks() {
+        clicks++;
+    }
+
+    public void incViews() {
+        views++;
+    }
+
+    public static String help() {
+        Field[] fields = Advertiser.class.getFields();
+        StringBuilder fieldNames = new StringBuilder("");
+        for (Field field : fields) {
+            fieldNames.append(field.getType() + " " + field.getName() + "\n");
+        }
+        return fieldNames.toString();
+    }
+
+    public String describeMe(){
+        return "Advertiser class";
+    }
+
+    public static int getTotalClicks() {
+        return totalClicks;
+    }
 }
