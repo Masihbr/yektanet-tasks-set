91c7c616a2c64c91af8b4449a8892eb3dc16d471
Masihbr <masihbr@gmail.com>
2021-01-07 17:06:28 +0330

    initial setup for Ad class

--- OOP-Java/src/Ad.java
+++ OOP-Java/src/Ad.java
@@ -5,6 +5,52 @@ public class Ad {
     private String link;
     private int clicks;
     private int views;
+    private Advertiser advertiser;
 
-    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setAdvertiser(Advertiser advertiser) {
+        this.advertiser = advertiser;
+    }
+
+    public int getClicks() {
+        return clicks;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void incClicks() {
+        clicks++;
+        advertiser.incClicks();
+    }
+
+    public void incViews() {
+        views++;
+        advertiser.incViews();
+    }
+
+    public String describeMe(){
+        return "Ad class";
+    }
+
 }
